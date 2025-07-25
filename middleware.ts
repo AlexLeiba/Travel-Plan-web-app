4d6c826e9a671aae467ce4d20fb12e470f5d56fd
Alex <alexandru@enovatika.com>
2025-07-25 17:28:00 +0300

    fix

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,20 @@ import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 
 // Define routes that need protection
-const protectedRoutes = ['/my-trips', '/globe', '/dashboard'];
+// const protectedRoutes = ['/my-trips', '/globe', '/dashboard'];
 
 export async function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
 
-  // Check if this is a route that needs auth
-  const requiresAuth = protectedRoutes.some((path) =>
-    pathname.startsWith(path)
-  );
+  // ⛔️ Bypass NextAuth routes
+  if (pathname.startsWith('/api/auth')) return NextResponse.next();
 
-  if (!requiresAuth) return NextResponse.next();
+  // // Check if this is a route that needs auth
+  // const requiresAuth = protectedRoutes.some((path) =>
+  //   pathname.startsWith(path)
+  // );
+
+  // if (!requiresAuth) return NextResponse.next();
 
   // Retrieve the token from cookies
   const token =
@@ -27,3 +30,7 @@ export async function middleware(req: NextRequest) {
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ['/my-trips/:path*', '/globe/:path*', '/dashboard/:path*'],
+};
