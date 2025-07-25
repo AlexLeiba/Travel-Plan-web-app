125a34633d803c16000eb9b8d5ee7e29caaa01ec
Alex <alexandru@enovatika.com>
2025-07-25 17:23:27 +0300

    fix

--- middleware.ts
+++ middleware.ts
@@ -15,7 +15,9 @@ export async function middleware(req: NextRequest) {
   if (!requiresAuth) return NextResponse.next();
 
   // Retrieve the token from cookies
-  const token = req.cookies.get('next-auth.session-token')?.value;
+  const token =
+    req.cookies.get('next-auth.session-token')?.value || // dev
+    req.cookies.get('__Secure-next-auth.session-token')?.value; // prod
 
   if (!token) {
     const loginUrl = new URL('/', req.url);
