3f1121be6e889c89a33ff469fa9a7b8605e4d171
AlexLeiba <Alexleiba13@gmail.com>
2025-10-27 13:44:59 +0200

    test env

--- auth.ts
+++ auth.ts
@@ -48,11 +48,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET || "",
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
 });
 
 export async function getServerSession() {
